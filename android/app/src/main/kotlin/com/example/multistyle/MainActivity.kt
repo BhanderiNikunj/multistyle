88ff6b0024597800bf7c6e9b006e35234b3b45a4
rw <nikunjbhanderi135@gmail.com>
2022-12-09 12:01:50 +0530

    multi style

---
+++ android/app/src/main/kotlin/com/example/multistyle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multistyle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "                 Nikunj Bhanderi\n",
+                        style: TextStyle(fontSize: 20)
+                      ),
+                      TextSpan(
+                        text: "           Nikunj Bhanderi\n",
+                        style: TextStyle(fontSize: 25)
+                      ),
+                      TextSpan(
+                        text: "       Nikunj Bhanderi\n",
+                        style: TextStyle(fontSize: 30)
+                      ),
+                      TextSpan(
+                          text: "    Nikunj Bhanderi\n",
+                          style: TextStyle(fontSize: 35)
+                      ),
+                      TextSpan(
+                          text: "  Nikunj Bhanderi\n",
+                          style: TextStyle(fontSize: 40)
+                      ),
+                      TextSpan(
+                          text: "Nikunj Bhanderi\n",
+                          style: TextStyle(fontSize: 45)
+                      ),TextSpan(
+                        text: "                 Nikunj Bhanderi\n",
+                        style: TextStyle(fontSize: 20)
+                      ),
+                      TextSpan(
+                        text: "              Nikunj Bhanderi\n",
+                        style: TextStyle(fontSize: 22)
+                      ),
+                      TextSpan(
+                          text: "            Nikunj Bhanderi\n",
+                          style: TextStyle(fontSize: 24)
+                      ),
+                      TextSpan(
+                          text: "          Nikunj Bhanderi\n",
+                          style: TextStyle(fontSize: 26)
+                      ),
+                      TextSpan(
+                          text: "        Nikunj Bhanderi\n",
+                          style: TextStyle(fontSize: 28)
+                      ),
+                      TextSpan(
+                          text: "      Nikunj Bhanderi\n",
+                          style: TextStyle(fontSize: 30)
+                      ),
+
+
+
+
+
+
+
+
+                      TextSpan(
+                        text: "\n\nnikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade50)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade100)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade200)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade300)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade400)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade500)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade600)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade700)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade800)
+                      ),
+                      TextSpan(
+                        text: "nikunj bhanderi\n",
+                        style: TextStyle(fontSize: 30,color: Colors.blue.shade900)
+                      ),
+                    ]
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
